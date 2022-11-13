b916eeed574a0805d5109cfaf1212f440e78e7dc
Jasper Schröder <jasper.schroder@gmail.com>
2022-11-13 10:54:20 +0000

    added progress indicator

--- electrum_setup.sh
+++ electrum_setup.sh
@@ -3,20 +3,25 @@
 # test seed: tube feed fringe old toward usual again tell switch bone fortune train
 
 PASSWORD="TEMP_PASS_123_!"
+PROGRESS=0
 
 rm -rf /home/pi/.electrum/
-
-electrum daemon -d
 
 if (whiptail --title "Electrum wallet" --yesno "Do you already have an Electrum wallet?" 8 78); then
         SEED=$(whiptail --title "Electrum seed" --inputbox "Enter your Electrum seed" 20 78 3>&1 1>&2 2>&3)
 	echo "Restoring Electrum wallet... $SEED"
-	electrum restore --password "$PASSWORD" "$SEED"
+	electrum --offline restore --password "$PASSWORD" "$SEED" | whiptail --title "Setting up Electrum" --gauge "Restoring wallet..." 20 40 $PROGRESS
 	SEED=""
 else
 	echo "Creating Electrum wallet..."
-        electrum create --password "$PASSWORD"
+        electrum --offline create --password "$PASSWORD" | whiptail --title "Setting up Electrum" --gauge "Creating wallet..." 20 40 $PROGRESS
 fi
 
-electrum load_wallet --password "$PASSWORD"
-electrum getmpk
+PROGRESS=20
+electrum daemon -d | whiptail --title "Setting up Electrum" --gauge "Starting daemon..." 20 40 $PROGRESS
+
+PROGRESS=40
+electrum load_wallet --password "$PASSWORD" | whiptail --title "Setting up Electrum" --gauge "Loading wallet..." 20 40 $PROGRESS
+
+PROGRESS=60
+electrum getmpk | whiptail --title "Setting up Electrum" --gauge "Retrieving master public key..." 20 40 $PROGRESS
