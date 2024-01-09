ad258f77b672a9608da96e762d2326ae84df8abf
Jasper Schroder <jasper.schroder@gmail.com>
2024-01-09 20:15:42 +0000

    flash led script

--- flash_led_start.sh
+++ flash_led_start.sh
@@ -8,10 +8,10 @@ do
         do
                 if "$LED_ON" == "true"; then
                         LED_ON="false"
-			echo 0 > /sys/class/leds/led0/brightness
+			sudo bash -c "echo 0 > /sys/class/leds/led0/brightness"
                 else
                         LED_ON="true"
-			echo 1 > /sys/class/leds/led0/brightness
+			sudo bash -c "echo 1 > /sys/class/leds/led0/brightness"
                 fi
                 sleep $var
         done
